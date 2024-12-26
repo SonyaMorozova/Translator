e98cf945d1396c688a6e640581f140c75498f7db
SonyaMorozova <154758247+SonyaMorozova@users.noreply.github.com>
2024-12-26 18:51:31 +0300

    Update test_translator.cpp

--- test/test_translator.cpp
+++ test/test_translator.cpp
@@ -1,46 +1,37 @@
 #include "translator.h"
 #include <gtest.h>
 
-TEST(Calculate, calculation_is_correct) {
-	Translator translator("((6*2-89*9)+2+3)*11");
-	translator.infix_to_postfix();
-	ASSERT_EQ(translator.calculate(), -8624);
+TEST(Calculate, can_correct_work_calculate_Translator) {
+	Translator translator("((6.2*2-89*9)+2+3)*11");
+	ASSERT_EQ( translator.calculate(), -8619.6);	
 }
 
-TEST(Calculate, can_get_postfix) {
+TEST(Calculate, can_get_postfix_Translator) {
 	Translator translator("((6*2-89*9)+2+3)*11");
-	translator.infix_to_postfix();
 	ASSERT_EQ(translator.get_postfix(), "62*899*-2+3+11*");
 }
 
-TEST(Calculate, can_get_infix) {
+TEST(Calculate, can_get_infix_Translator) {
 	Translator translator("((6*2-89*9)+2+3)*11");
 	ASSERT_EQ(translator.get_infix(), "((6*2-89*9)+2+3)*11");
 }
 
-TEST(Calculate, cant_division_by_zero) {
+TEST(Calculate, throw_division_by_zero_Translator) { 
 	Translator translator("11/0");
 	ASSERT_ANY_THROW(translator.calculate());
 }
 
-TEST(Calculate, throw_if_unknown_symbol) {
-	Translator translator("a/b");
+TEST(Calculate, throw_not_correct_input_2_Translator) {
+	Translator translator("((5+1)");
 	ASSERT_ANY_THROW(translator.calculate());
 }
 
-TEST(Calculate, throw_if_not_correct_input) {
-	Translator translator1(")5+1");
-	Translator translator2("((5+1)");
-	Translator translator3("*(5+1)");
-	Translator translator4("5++1");
-	Translator translator5("5+");
-	ASSERT_ANY_THROW(translator1.calculate());
-	ASSERT_ANY_THROW(translator2.calculate());
-	ASSERT_ANY_THROW(translator3.calculate());
-	ASSERT_ANY_THROW(translator4.calculate());
-	ASSERT_ANY_THROW(translator5.calculate());
-
+TEST(Calculate, throw_not_correct_input_3_Translator) {
+	Translator translator("*(5+1)");
+	ASSERT_ANY_THROW(translator.calculate());
 }
 
-
-
+TEST(Calculate, throw_not_correct_input_4_Translator) {
+	Translator translator("5++1");
+	ASSERT_ANY_THROW(translator.calculate());
+}
