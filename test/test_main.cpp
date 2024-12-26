9c329e3d3c169424c62fafb494952391ed1d46c9
SonyaMorozova <154758247+SonyaMorozova@users.noreply.github.com>
2024-12-26 18:50:33 +0300

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,7 +1,10 @@
 #include <gtest.h>
+#include "translator.h"
 
 int main(int argc, char** argv)
 {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	Begin();
+	return 0;
+	//::testing::InitGoogleTest(&argc, argv);
+	//return RUN_ALL_TESTS();
 }
